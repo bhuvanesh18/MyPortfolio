43b0472c3eb3be7e62343457a6e2d229781ecf98
Bhuvaneshwaran <bhuvaneshwaranmurugesh@gmail.com>
2020-04-23 14:45:33 +0530

    #7 fixed whatsapp number + name in home

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,8 @@ function Home() {
                 <div className="py-5">
                     <div className="row">
                         <div className="col-9 align-self-center">
-                            <h2>M Bhuvaneshwaran</h2>
+                            <h2 className="display-3 d-none d-sm-block d-md-block d-lg-block d-xl-block">M Bhuvaneshwaran</h2>
+                            <h2 className="d-block d-sm-none d-md-none d-lg-none d-xl-none">M Bhuvaneshwaran</h2>
                             <h6 className="text-warning">B.E Computer science and engineering</h6>
                             <h6><span>Mail ID : </span><a href="mailto:bhuvaneshwaranmurugesh@gmail.com" target="_target">bhuvaneshwaranmurugesh@gmail.com</a></h6>
                             <h6><span>Phone : </span><a href="tel:+919500352849">9500352849</a></h6>
--- src/components/SocialNetwork/SocialMediaCards.js
+++ src/components/SocialNetwork/SocialMediaCards.js
@@ -8,7 +8,7 @@ function SocialMediaCards() {
             <Card image_dir='social_media_icons/github.png' link='https://github.com/bhuvanesh18/' tip="Github"/>
             <Card image_dir='social_media_icons/facebook.png' link='https://www.facebook.com/bhuvanesh.bhuvanesh.9847' tip="Facebook"/>
             <Card image_dir='social_media_icons/instagram.jpg' link='https://www.instagram.com/_i_m_bhuvi_/' tip="Instagram"/>
-            <Card image_dir='social_media_icons/whatsapp.png' link='https://wa.me/9500352849' tip="Whatsapp"/>
+            <Card image_dir='social_media_icons/whatsapp.png' link='https://wa.me/919500352849' tip="Whatsapp"/>
         </React.Fragment>
     )
 }
