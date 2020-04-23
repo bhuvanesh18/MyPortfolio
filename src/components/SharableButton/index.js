5a53fce2cf2c3057e5c72ce5ca6d12077713b1ac
Bhuvaneshwaran <bhuvaneshwaranmurugesh@gmail.com>
2020-04-24 00:04:38 +0530

    #9 Share-Button-Issue

--- src/components/SharableButton/index.js
+++ src/components/SharableButton/index.js
@@ -1,16 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
+import $ from 'jquery';
 import ShareLogo from './../../img/social_media_icons/sharelogo.png';
 
-function index() {
-    return (
-        <React.Fragment>
-            <div className="fixed-bottom mb-3">
-                <div className="d-flex justify-content-end mr-2">
-                    <img src={ShareLogo} alt="Share Logo" className="btn" role="button" id="sharebutton" width="80px" />
+class index extends Component {
+
+    constructor(props) {
+        super(props)
+        this._shareClickHandler = this._shareClickHandler.bind(this);
+    }
+
+    componentDidMount(){
+        this._shareClickHandler();
+    }
+
+    _shareClickHandler(){
+        $('#sharebutton').on('click',function(){
+            console.log('Share Button Clicked!');
+            const title = window.document.title;
+            const url = window.document.location.href;
+
+            if(navigator.share){
+                console.log('Navigator Works');
+                navigator.share({
+                    title : `${title}`,
+                    url : `${url}`
+                }).then( () => {
+                    console.log('Thanks for sharing!!!');
+                })
+                .catch(console.error);
+            }else{
+                console.log('Nagivator Not Working!!');
+            }
+
+        })
+    } 
+
+    render() {
+        return (
+            <React.Fragment>
+                <div className="fixed-bottom mb-3">
+                    <div className="d-flex justify-content-end mr-2">
+                        <img src={ShareLogo} alt="Share Logo" className="btn" role="button" type="button" id="sharebutton" width="80px" height="70px"/>
+                    </div>
                 </div>
-            </div>
-        </React.Fragment>
-    )
+            </React.Fragment>
+        )
+    }
 }
 
 export default index
