a5692e33be60f1e7eed9870396d0b5abd5a44a47
Bhuvaneshwaran <bhuvaneshwaranmurugesh@gmail.com>
2020-04-25 12:40:54 +0530

    #17 Meta tags added in index.html and image name changed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ProfilePic from './../img/6.jpg';
+import ProfilePic from './../img/Bhuvaneshwaran.jpg';
 
 function Home() {
     return (
--- src/components/SideNav/SideNavProfile.js
+++ src/components/SideNav/SideNavProfile.js
@@ -5,7 +5,7 @@ function SideNavProfile() {
     <div id="side-nav" className="pl-5 py-5 container bg-primary border-bottom border-dark">
         <div className="pl-3 mx-auto">
             <div className="media">
-                <img src="./3.jpg" className="rounded-lg shadow-lg" alt="Profile" width="70%"/>
+                <img src="./Bhuvaneshwaran.jpg" className="rounded-lg shadow-lg" alt="Bhuvaneshwaran" width="70%"/>
             </div>
         </div>
     </div>
